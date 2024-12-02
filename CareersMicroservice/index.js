ace55c7e7efbf56c61c635ba56ddd930e6c02c75
Andrea Saldaña Cavour <andrea.saldana@alumnos.ucn.cl>
2024-12-02 00:26:11 -0300

    index.js created

---
+++ CareersMicroservice/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Importar rutas
+const careersRouter = require('./routes/careers');
+const subjectsRouter = require('./routes/subjects');
+
+// Inicializar aplicación
+const app = express();
+
+// Middleware para parsear JSON
+app.use(bodyParser.json());
+
+// Conectar a MongoDB
+mongoose.connect('mongodb://localhost:27017/carrerasDB')
+    .then(() => console.log('MongoDB conectado exitosamente'))
+    .catch(err => console.error('Error conectando a MongoDB:', err));
+
+
+// Rutas
+app.use('/careers', careersRouter); // Rutas de carreras
+app.use('/subjects', subjectsRouter); // Rutas de asignaturas
+
+// Iniciar el servidor
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Microservicio de Carreras corriendo en el puerto ${PORT}`);
+});
