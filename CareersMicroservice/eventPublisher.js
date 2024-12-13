5705e7974c064ea443a92d1faf49d409288f30e9
Andrea Saldaña Cavour <andrea.saldana@alumnos.ucn.cl>
2024-12-13 19:01:12 -0300

    fix eventPublisher file from careers microservice

--- CareersMicroservice/eventPublisher.js
+++ CareersMicroservice/eventPublisher.js
@@ -3,19 +3,25 @@ const amqp = require('amqplib');
 let channel;
 
 const connectRabbitMQ = async () => {
-  const connection = await amqp.connect('amqp://localhost');
-  channel = await connection.createChannel();
-  console.log('Conectado a RabbitMQ');
+    try {
+        const connection = await amqp.connect('amqp://localhost:5672');
+        channel = await connection.createChannel();
+        console.log('Conectado a RabbitMQ');
+    } catch (err) {
+        console.error('Error conectando a RabbitMQ:', err);
+    }
 };
 
 const publishEvent = async (queue, message) => {
-  if (!channel) {
-    console.error('El canal RabbitMQ no está configurado');
-    return;
-  }
-  await channel.assertQueue(queue, { durable: true });
-  channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
-  console.log(`Evento enviado a la cola ${queue}:`, message);
+    if (!channel) {
+        console.error('El canal RabbitMQ no está configurado');
+        return;
+    }
+    await channel.assertQueue(queue, { durable: true });
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+    console.log(`Evento enviado a la cola ${queue}:`, message);
 };
 
-module.exports = { connectRabbitMQ, publishEvent };
+connectRabbitMQ();
+
+module.exports = { publishEvent, connectRabbitMQ };
