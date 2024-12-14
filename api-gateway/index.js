3469a9d5a12927e35cad03d215d9aa088a10822d
coralinegrl <andrea.saldana@alumnos.ucn.cl>
2024-12-14 02:16:27 -0300

    fix api gateway index file

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,14 +1,56 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path');
+
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const PROTO_PATH = path.resolve(__dirname, '../CareersMicroservice/protos/career.proto');
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+
+const careerProto = grpc.loadPackageDefinition(packageDefinition).career;
+const client = new careerProto.CareerService(
+    'localhost:50051',
+    grpc.credentials.createInsecure()
+);
+
 // Middleware de prueba
 app.use((req, res, next) => {
     console.log(`Solicitud recibida en API Gateway: ${req.method} ${req.originalUrl}`);
     next();
+});
+
+// Rutas gRPC para el Microservicio de Carreras
+app.get('/careers', (req, res) => {
+    client.ListCareers({}, (error, response) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      res.json(response);
+    });
+});
+
+app.get('/careers/:id', (req, res) => {
+    const { id } = req.params;
+    client.GetCareer({ id }, (error, response) => {
+      if (error) {
+        console.error('Error en la llamada gRPC:', error);
+        return res.status(500).send(error);
+      }
+      res.json(response);
+    });
 });
 
 // Proxy hacia el Microservicio de Acceso
@@ -18,6 +60,9 @@ app.use('/auth', createProxyMiddleware({
     logLevel: 'debug', // Nivel de depuración para ver más detalles
     pathRewrite: { '^/auth': '/auth' } // Asegura que el path no se altera
 }));
+
+
+  
 
 // Iniciar el servidor
 app.listen(PORT, () => {
