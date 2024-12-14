523bf55e531425835f50b10c893e42b9eee69060
coralinegrl <andrea.saldana@alumnos.ucn.cl>
2024-12-14 02:20:09 -0300

    create auth middleware for CareersMicroservice

---
+++ CareersMicroservice/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware para autenticar el token JWT
+exports.authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token no proporcionado' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Token no válido' });
+        }
+        req.user = user;
+        next();
+    });
+};
