a2c0c3d35810eeb8994c08231b58c17b7359c1ed
coralinegrl <andrea.saldana@alumnos.ucn.cl>
2024-12-15 00:41:44 -0300

    add new forms to connect api gateway with careers microservice

--- api-gateway/src/routes/routes.js
+++ api-gateway/src/routes/routes.js
@@ -2,11 +2,31 @@ const express = require('express');
 const router = express.Router();
 const { routeToUserService, routeToMonolith } = require('../controllers/gatewayController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const careerClient = require('../grpc/careerClient');
 
 // Rutas para User Service (usando gRPC)
 router.use('/user-service', authMiddleware, routeToUserService);
 
 // Rutas para el Monolito (usando HTTP)
 router.use('/monolith', routeToMonolith);
+
+router.get('/', (req, res) => {
+    res.send('API Gateway funcionando correctamente');
+});
+
+// gRPC: Obtener una carrera por ID
+router.get('/careers/:id', (req, res) => {
+    const { id } = req.params;
+    careerClient.GetCareer({ id }, (error, response) => {
+        if (error) {
+            console.error('Error en la llamada gRPC:', error);
+            return res.status(500).send(error);
+        }
+        res.json(response);
+    });
+});
+
+
+
 
 module.exports = router;
