ca6ca7e3a8f7b4ef730da7863f22a805b5958002
coralinegrl <andrea.saldana@alumnos.ucn.cl>
2024-12-15 00:39:11 -0300

    fix new form to apply login endpoint with log

--- AccessMicroservice/routes/auth.js
+++ AccessMicroservice/routes/auth.js
@@ -4,7 +4,11 @@ const { register, login, updatePassword, logout } = require('../controllers/auth
 
 // Definir las rutas con los controladores correctos
 router.post('/register', register);
-router.post('/login', login);
+router.post('/login', (req, res, next) => {
+    console.log('Solicitud recibida en /auth/login:', req.body);
+    next();
+}, login);
+
 router.put('/update-password', updatePassword);
 router.post('/logout', logout);
 
